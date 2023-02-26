b5a634ca45e5ddd195c6cd94fb43bb6ee3e329d7
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-02-26 23:24:37 +0800

    page init

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:vr_movie_neon/eternal_page/eternalpage.dart';
 import 'movies_selection/selectionpage.dart';
 import 'homepage/homepage.dart';
 
 main() {
   runApp(
     MaterialApp(
-      home: SelectionPage(),
+      home: EternalPage(),
     ),
   );
 }
