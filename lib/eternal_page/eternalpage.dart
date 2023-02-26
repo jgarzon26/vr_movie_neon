ae6f6de55d62f852083024fc0580b77fc836c364
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-02-26 23:33:45 +0800

    appbar_layout

--- lib/eternal_page/eternalpage.dart
+++ lib/eternal_page/eternalpage.dart
@@ -6,6 +6,27 @@ class EternalPage extends StatelessWidget {
   Widget build (BuildContext context){
 
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: ImageIcon(
+            AssetImage('lib/eternal_page/icons/back.png'),
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: ImageIcon(
+              AssetImage('lib/eternal_page/icons/menu.png'),
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
       
     );
   }
